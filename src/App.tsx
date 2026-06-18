81d33cb48a38386d52fa4c2ffe9ce08531f63ab3
Prestoliv <hello@prestoliv.com>
2026-06-18 14:18:57 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -14,53 +14,56 @@ import CommercialPage from "./pages/pages_services/CommercialPage.tsx";
 import ResidentialPage from "./pages/pages_services/ResidentialPage.tsx";
 import InteriorsPage from "./pages/pages_services/InteriorsPage.tsx";
 import ConstructionCalculatorPage from "./pages/ConstructionCalculator.tsx";
+import Blog from "./pages/Blog.tsx";
+import BlogPost from "./pages/BlogPost.tsx";
 import { AnalyticsProvider } from "@/components/AnalyticsProvider";
 import { ContactSettingsProvider } from "@/components/ContactSettingsProvider";
 import { CookieConsentBanner } from "@/components/CookieConsentBanner";
 import { WhatsAppStickyButton } from "@/components/WhatsAppStickyButton";
 
-
 const queryClient = new QueryClient();
 
 const ScrollToTop = () => {
-  const { pathname } = useLocation();
+const { pathname } = useLocation();
 
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
+useEffect(() => {
+window.scrollTo(0, 0);
+}, [pathname]);
 
-  return null;
+return null;
 };
 
 const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <ContactSettingsProvider>
-        <AnalyticsProvider>
-        <ScrollToTop />
-        <CookieConsentBanner />
-        <WhatsAppStickyButton />
-        <Routes>
-          <Route path="/" element={<Index />} />
-        <Route path="/process" element={<Process />} />
-        <Route path="/services" element={<OurServices />} />
-        <Route path="/calculator" element={<ConstructionCalculatorPage />} />
-        <Route path="/services/commercial" element={<CommercialPage />} />
-        <Route path="/services/residential" element={<ResidentialPage />} />
-        <Route path="/services/interiors" element={<InteriorsPage />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/auth/callback" element={<AuthCallback />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-        </AnalyticsProvider>
-        </ContactSettingsProvider>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
+<QueryClientProvider client={queryClient}>
+<TooltipProvider>
+<Toaster />
+<Sonner />
+<BrowserRouter>
+<ContactSettingsProvider>
+<AnalyticsProvider>
+<ScrollToTop />
+<CookieConsentBanner />
+<WhatsAppStickyButton />
+<Routes>
+<Route path="/" element={<Index />} />
+<Route path="/process" element={<Process />} />
+<Route path="/services" element={<OurServices />} />
+<Route path="/calculator" element={<ConstructionCalculatorPage />} />
+<Route path="/services/commercial" element={<CommercialPage />} />
+<Route path="/services/residential" element={<ResidentialPage />} />
+<Route path="/services/interiors" element={<InteriorsPage />} />
+<Route path="/about" element={<About />} />
+<Route path="/blog" element={<Blog />} />
+<Route path="/blog/:slug" element={<BlogPost />} />
+<Route path="/auth/callback" element={<AuthCallback />} />
+{/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+<Route path="*" element={<NotFound />} />
+</Routes>
+</AnalyticsProvider>
+</ContactSettingsProvider>
+</BrowserRouter>
+</TooltipProvider>
+</QueryClientProvider>
 );
 
 export default App;
